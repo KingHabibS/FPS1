e3d8d1648b019560de56e0f0d468300a312ab9aa
KingHabibS <habboobsaliva19@gmail.com>
2022-06-19 16:05:32 +0300

    Reload Animation

--- Learning Game 1/Assets/Scripts/Guns/Gun.cs
+++ Learning Game 1/Assets/Scripts/Guns/Gun.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class Gun : MonoBehaviour
 {
@@ -8,15 +9,35 @@ public class Gun : MonoBehaviour
     public float range = 100f;
     public float fireRate = 15f;
 
+    public int maxAmmo = 10;
+    private int currentAmmo;
+    public float reloadTime = 1f;
+    private bool isReloading = false;
+
     public Camera fpsCam;
     public ParticleSystem flash;
     public GameObject impactEffect;
 
     private float nextTimeToFire = 0f;
 
+    void Start ()
+    {
+        if (currentAmmo == -1)
+            currentAmmo = maxAmmo;
+    }
+
     // Update is called once per frame
     void Update()
     {
+        if (isReloading)
+            return;
+        
+        if (currentAmmo <= 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+
         if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
         {
             nextTimeToFire = Time.time + 1f / fireRate;
@@ -25,9 +46,22 @@ public class Gun : MonoBehaviour
 
     }
 
+    IEnumerator Reload ()
+    {
+        isReloading = true;
+        Debug.Log("Reloading...");
+
+        yield return new WaitForSeconds(reloadTime);
+
+        currentAmmo = maxAmmo;
+        isReloading = false;
+    }
+
     void Shoot()
     {
         flash.Play();
+
+        currentAmmo--;
 
         RaycastHit hit;
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
